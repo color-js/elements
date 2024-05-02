603e2b4f4cedf3cc46145296945268c70dad8f4d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-05-02 19:44:34 +0200

    Fix the Nude UI components' URL

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ document.addEventListener("scroll", evt => {
 	root.style.setProperty("--scrolltop", root.scrollTop);
 }, {passive: true});
 
-import HTMLDemoElement from "https://nudeui.com/html-demo/html-demo.js";
+import HTMLDemoElement from "https://nudeui.com/elements/html-demo/html-demo.js";
 
 if (document.body.classList.contains("component-page")) {
 	HTMLDemoElement.wrapAll();
