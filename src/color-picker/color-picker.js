3afc0c6f18bde2f6f1a4b157bfcb4196b35c99a6
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-05-03 14:53:10 +0200

    Rename: swatch → size (#8)
    One thing to consider yet: Should swatch="none" remain? Yes, for now. size="none" sounds a bit weird.

--- src/color-picker/color-picker.js
+++ src/color-picker/color-picker.js
@@ -19,7 +19,7 @@ const self = class ColorPicker extends HTMLElement {
 			<style>@import url("${ styleURL }")</style>
 			<div id=sliders></div>
 			<slot name="swatch">
-				<color-swatch swatch="large" id="swatch">
+				<color-swatch size="large" id="swatch">
 					<input value="oklch(70% 0.25 138)" id="color" />
 				</color-swatch>
 			</slot>
