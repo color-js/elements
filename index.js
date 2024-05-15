b77e94d761ef5027219af958bb741b8891bb3a57
Lea Verou <lea@verou.me>
2024-05-15 12:59:28 -0400

    Reorder components from highest to lowest level

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export { default as ColorInline } from "./src/color-inline/color-inline.js";
-export { default as ColorSwatch } from "./src/color-swatch/color-swatch.js";
-export { default as ColorSlider } from "./src/color-slider/color-slider.js";
-export { default as ChannelSlider } from "./src/channel-slider/channel-slider.js";
 export { default as ColorPicker } from "./src/color-picker/color-picker.js";
+export { default as ColorSwatch } from "./src/color-swatch/color-swatch.js";
+export { default as ColorInline } from "./src/color-inline/color-inline.js";
+export { default as ChannelSlider } from "./src/channel-slider/channel-slider.js";
+export { default as ColorSlider } from "./src/color-slider/color-slider.js";
 export { default as ColorGamut } from "./src/color-gamut/color-gamut.js";
