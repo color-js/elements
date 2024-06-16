5716a8437993940bc27358cb12f19dbee234cab8
Lea Verou <lea@verou.me>
2024-06-16 04:58:36 +0300

    [color-chart] Basic axes

--- src/color-chart/README.md
+++ src/color-chart/README.md
@@ -26,3 +26,15 @@ By default, the other coordinate would be the index of the color in the list, bu
 	<color-scale colors="50: #e3f2fd, 100: #bbdefb, 200: #90caf9, 300: #64b5f6, 400: #42a5f5, 500: #2196f3, 600: #1e88e5, 700: #1976d2, 800: #1565c0, 900: #0d47a1"></color-scale>
 </color-chart>
 ```
+
+You can also specify a whole label, and if it contains a number, the number will become the X coordinate:
+
+```html
+<color-chart y="oklch.l">
+	<color-scale colors="Red 50: #fef2f2, Red 100: #fee2e2, Red 200: #fecaca, Red 300: #fca5a5, Red 400: #f87171, Red 500: #ef4444, Red 600: #dc2626, Red 700: #b91c1c, Red 800: #991b1b, Red 900: #7f1d1d, 950: #450a0a"></color-scale>
+	<color-scale colors="Orange 50: #fff7ed, Orange 100: #ffedd5, Orange 200: #fed7aa, Orange 300: #fdba74, Orange 400: #fb923c, Orange 500: #f97316, Orange 600: #ea580c, Orange 700: #c2410c, Orange 800: #9a3412, Orange 900: #7c2d12, Orange 950: #431407"></color-scale>
+	<color-scale colors="Yellow 50: #fefce8, Yellow 100: #fef9c3, Yellow 200: #fef08a, Yellow 300: #fde047, Yellow 400: #facc15, Yellow 500: #eab308, Yellow 600: #ca8a04, Yellow 700: #a16207, Yellow 800: #854d0e, Yellow 900: #713f12, Yellow 950: #422006"></color-scale>
+</color-chart>
+```
+
+_(Colors courtesy of Tailwind)_
--- src/color-chart/color-chart.js
+++ src/color-chart/color-chart.js
@@ -14,14 +14,20 @@ const Self = class ColorChart extends NudeElement {
 		let styleURL = new URL(`./${Self.tagName}.css`, import.meta.url);
 		this.shadowRoot.innerHTML = `
 			<style>@import url("${ styleURL }")</style>
-			<div id="chart">
-				<slot></slot>
+			<div id="chart-container">
+				<div id="chart">
+					<slot></slot>
+				</div>
 			</div>
+			<div class="axis" id="y_axis"></div>
+			<div class="axis" id="x_axis"></div>
 		`;
 
 		this._el = {
 			slot: this.shadowRoot.querySelector("slot"),
 			chart: this.shadowRoot.getElementById("chart"),
+			yAxis: this.shadowRoot.getElementById("y_axis"),
+			xAxis: this.shadowRoot.getElementById("x_axis"),
 		};
 	}
 
@@ -41,7 +47,7 @@ const Self = class ColorChart extends NudeElement {
 		let minY = Infinity, maxY = -Infinity;
 
 		for (let colorScale of this.querySelectorAll("color-scale")) {
-			let prevY, prevX, prevColor;
+			let prevColor;
 			let i = 0;
 
 			colorScale.style.setProperty("--color-count", colorScale.computedColors.length);
@@ -71,10 +77,19 @@ const Self = class ColorChart extends NudeElement {
 			}
 		}
 
-		this._el.chart.style.setProperty("--min-x", minX);
-		this._el.chart.style.setProperty("--max-x", maxX);
-		this._el.chart.style.setProperty("--min-y", minY);
-		this._el.chart.style.setProperty("--max-y", maxY);
+		let yAxis = getAxis(minY, maxY, 10);
+		let xAxis = getAxis(minX, maxX, 10);
+
+		this._el.chart.style.setProperty("--min-x", xAxis.min);
+		this._el.chart.style.setProperty("--max-x", xAxis.max);
+		this._el.chart.style.setProperty("--steps-x", xAxis.steps);
+
+		this._el.chart.style.setProperty("--min-y", yAxis.min);
+		this._el.chart.style.setProperty("--max-y", yAxis.max);
+		this._el.chart.style.setProperty("--steps-y", yAxis.steps);
+
+		this._el.xAxis.innerHTML = Array(xAxis.steps).fill().map((_, i) => "<div>" + +(xAxis.min + i * xAxis.step).toPrecision(15) + "</div>").join("\n");
+		this._el.yAxis.innerHTML = Array(yAxis.steps).fill().map((_, i) => "<div>" + +(yAxis.min + i * yAxis.step).toPrecision(15) + "</div>").reverse().join("\n");
 	}
 
 	propChangedCallback ({name, prop, detail: change}) {
@@ -108,3 +123,28 @@ const Self = class ColorChart extends NudeElement {
 customElements.define(Self.tagName, Self);
 
 export default Self;
+
+function getAxis (min, max, initialSteps) {
+	let range = max - min;
+	let step = range / initialSteps;
+	let magnitude = Math.floor(Math.log10(step));
+	let base = Math.pow(10, magnitude);
+	let candidates = [base, base * 2, base * 5, base * 10];
+
+	for (let i = 0; i < candidates.length; i++) {
+		if (candidates[i] > step) {
+			step = candidates[i];
+			break;
+		}
+	}
+
+	let start = Math.floor(min / step) * step;
+	let end = Math.ceil(max / step) * step;
+	let steps = Math.round((end - start) / step);
+
+	let ret = {min: start, max: end, step, steps};
+	for (let prop in ret) {
+		ret[prop] = +ret[prop].toPrecision(15);
+	}
+	return ret;
+}
