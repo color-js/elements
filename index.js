827046c471fadb358aa6244b8d315794a0646b4e
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-10-18 17:22:22 +0200

    [index.js] Add `<color-chart>` to the list of components
    Even though this file is a build artifact, it can't be gitignored since it's the module entry point.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export { default as ColorPicker } from "./src/color-picker/color-picker.js";
 export { default as ColorScale } from "./src/color-scale/color-scale.js";
+export { default as ColorChart } from "./src/color-chart/color-chart.js";
 export { default as ColorSwatch } from "./src/color-swatch/color-swatch.js";
 export { default as ColorInline } from "./src/color-inline/color-inline.js";
 export { default as ChannelSlider } from "./src/channel-slider/channel-slider.js";
