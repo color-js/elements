06146b2390d6d73e2252531c65f42ccf5ccd44f7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-10-29 12:12:36 +0100

    Make the website more performant
    Remove the `scroll` event listener (it came from the Color.js website, where we now use a different approach to position the menu).

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,6 @@ import "https://colorjs.io/assets/js/colors.js";
 import { styleCallouts } from "https://colorjs.io/assets/js/enhance.js";
 styleCallouts();
 
-let root = document.documentElement;
-document.addEventListener("scroll", evt => {
-	root.style.setProperty("--scrolltop", root.scrollTop);
-}, {passive: true});
-
 import HTMLDemoElement from "https://nudeui.com/elements/html-demo/html-demo.js";
 
 if (document.body.classList.contains("component-page")) {
