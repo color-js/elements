68343dc1f88f7481927a2b878fe4838422ba44b1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-11-07 11:45:06 +0100

    Use getter

--- src/channel-picker/channel-picker.js
+++ src/channel-picker/channel-picker.js
@@ -41,7 +41,7 @@ const Self = class ChannelPicker extends ColorElement {
 	}
 
 	#render () {
-		let space = this._el.space_picker.selectedSpace;
+		let space = this.selectedSpace;
 		let coords = space?.coords;
 
 		if (space && !coords) {
