40d39195b7ad8399f49dca823ad3b2bde916eef6
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-12-05 23:08:28 +0100

    Fix tab order

--- src/color-scale/color-scale.js
+++ src/color-scale/color-scale.js
@@ -292,22 +292,6 @@ const Self = class ColorScale extends ColorElement {
 			swatch.classList[intermediate ? "add" : "remove"]("intermediate");
 
 			if (!intermediate) {
-				let deleteButton = swatch.querySelector("[part=delete-button]");
-				if (this.editable?.delete) {
-					if (!deleteButton) {
-						swatch.insertAdjacentHTML("beforeend", `
-							<button part="delete-button" title="Delete color">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-									<path fill="currentColor" fill-rule="evenodd" d="m18.412 6.5l-.801 13.617A2 2 0 0 1 15.614 22H8.386a2 2 0 0 1-1.997-1.883L5.59 6.5H3.5v-1A.5.5 0 0 1 4 5h16a.5.5 0 0 1 .5.5v1zM10 2.5h4a.5.5 0 0 1 .5.5v1h-5V3a.5.5 0 0 1 .5-.5M9 9l.5 9H11l-.4-9zm4.5 0l-.5 9h1.5l.5-9z" />
-								</svg>
-							</button>
-						`);
-					}
-				}
-				else {
-					deleteButton?.remove();
-				}
-
 				// Make the swatch editable, if needed
 				let editable;
 				if (this.editable?.name) {
@@ -322,6 +306,22 @@ const Self = class ColorScale extends ColorElement {
 				}
 				else {
 					swatch.editable = false;
+				}
+
+				let deleteButton = swatch.querySelector("[part=delete-button]");
+				if (this.editable?.delete) {
+					if (!deleteButton) {
+						swatch.insertAdjacentHTML("beforeend", `
+							<button part="delete-button" title="Delete color">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path fill="currentColor" fill-rule="evenodd" d="m18.412 6.5l-.801 13.617A2 2 0 0 1 15.614 22H8.386a2 2 0 0 1-1.997-1.883L5.59 6.5H3.5v-1A.5.5 0 0 1 4 5h16a.5.5 0 0 1 .5.5v1zM10 2.5h4a.5.5 0 0 1 .5.5v1h-5V3a.5.5 0 0 1 .5-.5M9 9l.5 9H11l-.4-9zm4.5 0l-.5 9h1.5l.5-9z" />
+								</svg>
+							</button>
+						`);
+					}
+				}
+				else {
+					deleteButton?.remove();
 				}
 			}
 
