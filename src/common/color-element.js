e6494835c0d257cc2311f9fec101c9d8fd6f21ed
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-03-06 18:33:40 +0100

    Update import of Color.js

--- src/common/color-element.js
+++ src/common/color-element.js
@@ -1,6 +1,7 @@
-import NudeElement from "../../node_modules/nude-element/src/index.js";
-import { states } from "../../node_modules/nude-element/src/plugins/index.js";
-import { getType, defer, wait, dynamicAll, noOpTemplateTag as css } from "./util.js";
+import Color from "colorjs.io";
+import NudeElement from "nude-element";
+import { states } from "nude-element/plugins";
+import { defer, wait, dynamicAll, noOpTemplateTag as css } from "./util.js";
 
 const baseGlobalStyles = css`
 	@keyframes fade-in {
@@ -86,32 +87,7 @@ const Self = class ColorElement extends NudeElement {
 		// Give other code a chance to overwrite Self.Color
 		await wait();
 
-		if (!Self.Color) {
-			let specifier;
-
-			try {
-				// Is already loaded? (e.g. via an import map, or if we're in Node)
-				import.meta.resolve("colorjs.io");
-				specifier = "colorjs.io";
-			}
-			catch (e) {
-				// specifier = "../../node_modules/colorjs.io/dist/color.js";
-				specifier = "https://colorjs.io/dist/color.js";
-			}
-
-			Self.Color = import(specifier).then(module => module.default);
-		}
-
-		// We can't just use top level await, see https://bugs.webkit.org/show_bug.cgi?id=242740
-		if (getType(Self.Color) === "Promise") {
-			let ColorPending = Self.Color;
-			let Color = await ColorPending;
-
-			if (Self.Color === ColorPending) {
-				// Hasn't changed
-				Self.Color = Color;
-			}
-		}
+		Self.Color ??= Color;
 
 		customElements.define(this.tagName, this);
 
